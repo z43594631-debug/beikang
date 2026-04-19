942cc95c71a0624b129365a1b73670e8fd54e6f4
z43594631-debug <z43594631@gmail.com>
2026-04-19 21:15:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -95,3 +95,8 @@ def login(user: UserCreate):
 
     finally:
         db.close()
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
